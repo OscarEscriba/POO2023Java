55ae8ffb0fe85d076887b76cbddec2c134c35abb
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-08-01 09:54:42 -0600

    EstudioParte1 de Todo

---
+++ Torneo Platino/Equipo.java
@@ -0,0 +1,25 @@
+//las clases en java no llevan () 
+//las clases principales llevan el mismo nombre que el archivo donde son creadas. 
+public class Equipo {  
+    // el nombre del consutructor tiene que ser igual al el de la clase en la que se encuentra.  
+    //lo siguiente es un constructor.  
+     /* 
+      * constructor: metodo especial para crear e inicializar objeto en esa clase. Su principal funcion es asignar valores 
+      iniciales a las variables miembro (atributos o propiedades) del objeto cuando se crea una nueva istancia de la clase. 
+      */
+    public Equipo() { 
+    }  
+    /* 
+     * Son los atributos de la clase 
+     */
+    private String nombre; 
+    private int posicion; 
+    private int tirosEsquina; 
+    private int ganados; 
+    private int perdidos; 
+    private int tirosAgol; 
+    private int goles;
+    private int amarillas; 
+    private int faltas; 
+
+}
