5e2378a4f74b1f68f2f72df67f0c73649ba75b50
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-08-06 01:29:37 -0600

    update dado.java

---
+++ Laboratorio1OscarEscriba/Dado.java
@@ -0,0 +1,37 @@
+package Laboratorio1OscarEscriba; 
+import java.util.Random; 
+
+
+public class Dado { 
+    private int dado1; 
+    private int dado2; 
+    private Random GenerarRandom; 
+
+    //constructor de la clase dado. 
+    public Dado(int dado1, int dado2) {  
+        this.dado1 = dado1; 
+        this.dado2 = dado2; 
+        GenerarRandom = new Random(); 
+    } 
+// se crean los metodos para obtener los valores de los atributos de las clases. 
+    public int getdado1() { 
+        return this.dado1; 
+    } 
+    public void setdado1(int dado1) { 
+        this.dado1=dado1;  
+    } 
+    public int getdado2() { 
+        return this.dado2; 
+    } 
+    public void setdado2(int dado2) { 
+        this.dado2 =dado2; 
+    } 
+    public int Aleatorio() { 
+        return GenerarRandom.nextInt(6-1+1)+1; 
+    } 
+    //le asigna un valor a los dados para el juego. 
+    public void tiro(){ 
+        dado1=  Aleatorio(); 
+        dado2= Aleatorio(); 
+    }
+}
